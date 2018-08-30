9e5707f544d79cfaa8055cd87f4c21c50a4b0a2d
MaxHalleran <MaxHalleran@gmail.com>
2018-08-30 17:07:18 +0000

    The proper file without any experimentation

--- practiceObjMethods.js
+++ practiceObjMethods.js
@@ -1,27 +1,9 @@
-// // The second argument/parameter is expected to be a function
-// function findWaldo(arr, found) {
-//   for (var i = 0; i < arr.length; i++) {
-//     if (arr[i] === "Waldo") {
-//       found(i);   // execute callback
-//     }
-//   }
-// }
-
-// function actionWhenFound(index) {
-//   console.log("Found Waldo at index " + index + "!");
-// }
-
-// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-
-function findWaldo(arr, found)
- {
-  arr.forEach
-  (()=>
-    if (arr[i] === "Waldo")
-      {
-        found(i);
-      });
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);
+    }
+  }
 }
 
 function actionWhenFound(index) {
