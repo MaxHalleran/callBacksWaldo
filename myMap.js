77a851cd344758010859ef42e758b2a67c8d69ba
MaxHalleran <MaxHalleran@gmail.com>
2018-08-30 19:10:50 +0000

    Updated myMap.js

--- myMap.js
+++ myMap.js
@@ -1,4 +1,4 @@
-var map = function(arr, condition) {
+var myMap = function(arr, condition) {
   var resultant = [];
 
   arr.forEach( function(cell) {
@@ -11,20 +11,20 @@ var map = function(arr, condition) {
 
 // var words = ["ground", "control", "to", "major", "tom"];
 
-// map(words, function(word) {
+// myMap(words, function(word) {
 //   return word.length;
 // });
 
-// map(words, function(word) {
+// myMap(words, function(word) {
 //   return word.toUpperCase();
 // });
 
-// map(words, function(word) {
+// myMap(words, function(word) {
 //   return word.split('').reverse().join('');
 // });
 
-// // [6, 7, 2, 5, 3]
+// [6, 7, 2, 5, 3]
 
-// // [ "GROUND", "CONTROL", "TO", "MAJOR", "TOM" ]
+// [ "GROUND", "CONTROL", "TO", "MAJOR", "TOM" ]
 
-// // [ 'dnuorg', 'lortnoc', 'ot', 'rojam', 'mot' ]
+// [ 'dnuorg', 'lortnoc', 'ot', 'rojam', 'mot' ]
